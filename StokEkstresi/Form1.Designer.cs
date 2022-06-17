6d1e395027f76fc7fd1358ae263ebdd835195f9c
Saltukz <s.uzungungor@gmail.com>
2022-06-17 22:33:55 +0300

    final

--- StokEkstresi/Form1.Designer.cs
+++ StokEkstresi/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(504, 248);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(65, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(191, 83);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(197, 23);
+            this.dateTimePicker1.TabIndex = 5;
+
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(447, 83);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button button1;
+        private DataGridView dataGridView1;
+        private ListBox listBox1;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- StokEkstresi/Form1.cs
+++ StokEkstresi/Form1.cs
@@ -1,10 +1,90 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
 namespace StokEkstresi
 {
     public partial class Form1 : Form
     {
+
+        string connectionString = @"Server=SALTUK\MSSQLSALTUK;Database=Test;Trusted_Connection=True;";
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT DISTINCT MalKodu from STK";
+            SqlCommand command = new SqlCommand(query, connection);
+           
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["MalKodu"]);
+            }
+            connection.Close();
+
+
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+           
+            string query = "[TariheGoreStok]";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@malkodu", listBox1.Text);
+            command.Parameters.AddWithValue("@tarih1", Convert.ToInt32(dateTimePicker1.Value.ToOADate()));
+            command.Parameters.AddWithValue("@tarih2", Convert.ToInt32(dateTimePicker2.Value.ToOADate()));
+
+            DataTable table = new DataTable();
+            table.Columns.Add("SiraNo", typeof(string));
+            table.Columns.Add("IslemTur", typeof(short));
+            table.Columns.Add("EvrakNo", typeof(string));
+            table.Columns.Add("Tarih2", typeof(string));
+            table.Columns.Add("GirisMiktar", typeof(int));
+            table.Columns.Add("CikisMiktar", typeof(int));
+            table.Columns.Add("Stok", typeof(int));
+
+
+            connection.Open();
+
+          
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                row["SiraNo"] = (string)reader["SiraNo"];
+                row["IslemTur"] = reader["IslemTur"];
+                row["EvrakNo"] = reader["EvrakNo"];
+                row["Tarih2"] = reader["Tarih2"];
+                row["GirisMiktar"] = reader["GirisMiktar"];
+                row["CikisMiktar"] = reader["CikisMiktar"];
+                row["Stok"] = reader["Stok"];
+
+
+                table.Rows.Add(row);
+            }
+         
+            connection.Close();
+            command.Dispose();
+            connection.Dispose();
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
     }
 }
